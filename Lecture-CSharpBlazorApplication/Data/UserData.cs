9d946e8a2d20c92d05f80a5c92a8d37f7a4ad235
KrumiChan <lovelybarry@naver.com>
2021-09-08 20:51:09 +0900

    Binding을 이용한 여러 기능 연습.

---
+++ Lecture-CSharpBlazorApplication/Data/UserData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture_CSharpBlazorApplication.Data
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+    }
+}
