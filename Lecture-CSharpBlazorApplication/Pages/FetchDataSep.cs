6d486e8fd9fe621635c157acb4ac5391c3255fcf
KrumiChan <lovelybarry@naver.com>
2021-09-09 16:12:20 +0900

    Blazor Component와 Cs Class 분리.

---
+++ Lecture-CSharpBlazorApplication/Pages/FetchDataSep.cs
@@ -0,0 +1,20 @@
+﻿using Lecture_CSharpBlazorApplication.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture_CSharpBlazorApplication.Pages
+{
+    // razor component에서 @code { }를 분리하고 싶으면,
+    // razor와 class의 파일명을 맞추면 사용할 수 있다.
+    public partial class FetchDataSep
+    {
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
