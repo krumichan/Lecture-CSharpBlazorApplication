77311b471e84ca5ee47495207e18c048afbc23aa
KrumiChan <lovelybarry@naver.com>
2021-09-10 13:51:44 +0900

    State 관리 공부 및 간단 구현.

---
+++ BlazorStudyState/Data/CounterState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStudyState.Data
+{
+    public class CounterState
+    {
+        int _count = 0;
+
+        public Action OnStateChanged;
+
+        public int Count
+        {
+            get { return _count; }
+
+            set
+            {
+                _count = value;
+                Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+    }
+}
